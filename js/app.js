02c2364ca03a1de38fcc71e74c6b2e35c6daf8a6
Josh Owens <joshua.owens@gmail.com>
2011-04-24 09:01:00 -0400

    Version 0.0.1 of redesign, major WIP.

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+  $('#nav li').hover(function() { //mouse in
+      $(this).animate({ marginTop: '-38px' }, 900);
+    }, function() { //mouse out
+      $(this).animate({ marginTop: "0px" }, 900);
+  });
+
+  $('#footer .social li').hover(function() {
+    $(this).animate({ marginTop: '-20px'}, 900);
+  }, function() {
+    $(this).animate({ marginTop: '0px'}, 900);
+  });
+
+  $('.patrons, .about, .services, .identity, .contact').localScroll({hash: true});
+
+});
